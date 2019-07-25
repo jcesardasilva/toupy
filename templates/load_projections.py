f7a12f4d9c3e1ec8114712b6687c435a4d672a32
Julio Cesar DA SILVA <jcesardasilvaclean@gmail.com>
2019-07-25 22:26:31 +0200

    preparing templates: load_projections.py

--- templates/load_projections.py
+++ templates/load_projections.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Standard library imports
-import os
-import sys
-
 # third party packages
 import numpy as np
 
 # local packages
-from io_utils import checkhostname, LoadProjections, SaveData, create_paramsh5
+from toupy.io import LoadProjections, SaveData
 
 # initializing params
 params=dict()
 
 ### Edit section ###
 #=========================
-params[u'showrecons']=False
-params[u'account']=u'ma4352'
-params[u'samplename'] = u'gp2_NaCl_dif_pitch_ffp_tomo'
-params[u'pathfilename']=u'/data/id16a/inhouse4/visitor/ma4352/id16a/analysis/recons/gp2_NaCl_dif_pitch_ffp_tomo_div1p2_b2um_subtomo001_0000_/gp2_NaCl_dif_pitch_ffp_tomo_div1p2_b2um_subtomo001_0000__ML.ptyr'
-params[u'regime'] = 'farfield' #'nearfield' or 'farfield'
-params[u'autosave'] = True
-params[u'phaseonly'] = False # put false if you want to do the amplitude tomogram
-params[u'border_crop_x'] = None
-params[u'border_crop_y'] = None
-params[u'checkextraprojs'] = False
-params[u'missing_proj'] = False
-params[u'missing_num'] = []
-#params[u'cxientry'] = None
+params['account'] = 'ma4351'
+params['samplename'] = 'v97_v_nfptomo2_15nm'
+params['pathfilename']='/data/id16a/inhouse4/visitor/ma4351/id16a/analysis/recons/v97_v_nfptomo2_15nm_subtomo001_0000/v97_v_nfptomo2_15nm_subtomo001_0000_ML.ptyr'
+params['regime'] = 'nearfield' #'nearfield' or 'farfield'
+params['showrecons'] = False
+params['autosave'] = True
+params['phaseonly'] = False # put false if you want to do the amplitude tomogram
+params['border_crop_x'] = None
+params['border_crop_y'] = None
+params['checkextraprojs'] = True
+params['missingprojs'] = False
+params['missingnum'] = []
 #=========================
 
 #=============================================================================#
@@ -36,33 +31,10 @@ params[u'missing_num'] = []
 #=============================================================================#
 
 if __name__=='__main__':
-    #load the projections
-    host_machine = checkhostname()
-
-    #-------------------------------------------------------
-    # still keep this block, but it should disappear soon
-    if sys.version_info<(3,0):
-        input = raw_input
-    #-------------------------------------------------------
-
     #stack_objs,obj_shape,theta,pixelsize=load_projection(**params)
     P = LoadProjections(**params)
     stack_objs,theta,pixelsize = P()
     params['pixelsize'] = pixelsize
-
-    # create parameter file
-    create_paramsh5(**params)
-
-    # special: insert the information of the missing projections
-    if params[u'missing_proj']:
-        print('Inserting the missing projections')
-        missing = params[u'missing_num']
-        print(missing)
-        delta_theta = theta[1]-theta[0]
-        for ii in missing:
-            print('Projection: {}'.format(ii))
-            theta = np.insert(theta,ii,theta[ii-1]+delta_theta)
-            stack_objs = np.insert(stack_objs,ii,stack_objs[ii-1], axis=0)
 
     # Save reconstructed phase projections
     S = SaveData(**params)
