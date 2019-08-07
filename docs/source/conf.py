0692f6b9473fbd17118f62f60b44f60bc6acb796
Julio Cesar Da Silva <jdasilva@esrf.fr>
2019-08-07 18:21:58 +0200

    changes in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ for mod_name in MOCK_MODULES:
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 sys.path.insert(0, os.path.abspath('./../..'))
-sys.path.insert(0, os.path.abspath('./../../toupy/'))
+#sys.path.insert(0, os.path.abspath('./../../toupy/'))
 #sys.setrecursionlimit(1500)
 
 
