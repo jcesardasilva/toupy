128a5291b74675cda1e1464b9bd0461bc1c08d31
Julio Cesar Da Silva <jdasilva@esrf.fr>
2019-08-08 09:50:41 +0200

    third trial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,6 @@ sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../toupy'))
 #sys.setrecursionlimit(1500)
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'Toupy - Tomographic Utilities for Python'
