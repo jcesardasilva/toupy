beb21946a8d5b90c9229446bae13c23c64b11cc1
Julio Cesar DA SILVA <jcesardasilvaclean@gmail.com>
2019-08-13 18:00:27 +0200

    add rm vortices obj and probe

---
+++ templates/remove_vortices_object.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# standard packages
+import os
+import shutil
+import time
+
+# third party packages
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+
+# local package
+from toupy.io import read_recon
+from toupy.restoration import rmvortices_object
+
+# initializing dictionaries
+params = dict()
+
+# remove vortices from the probe
+# =========================
+params["filename"] = "/data/visitor/ma3495/id16a/analysis/recons/O2_LSCF_CGO_25nm_subtomo001_0000/O2_LSCF_CGO_25nm_subtomo001_0000_ML.ptyr"
+params["overwrite_h5"] = True  # True or False
+params["to_ignore"] = 100
+params["show_figures"] = True
+# =========================
+
+# =============================================================================#
+# Don't edit below this line, please                                          #
+# =============================================================================#
+if __name__ == "__main__":
+
+    # Open the file and extract the image data
+    # The orientation is not changed because the image is fed back into file
+    objimg, probe, pixelsize, energy = read_recon(
+        params["filename"], correct_orientation=False
+    )
+
+    # get the phase-shifts of the object
+    obj_phase = np.angle(objimg[0])
+
+    # Removing vortices
+    print("Removing vortices of object...")
+    obj_phase_novort, xres, yres = rmvortices_object(
+        objimg[0], to_ignore=params["to_ignore"]
+    )
+
+    # feed the new array
+    probe_novort = np.empty_like(probe)
+    for ii in range(n_probes):
+        probe_novort[ii] = p_phase_novort[ii]
+
+    # display the object with the residues and without vortices
+    if params["show_figures"]:
+        plt.close("all")
+        fig = plt.figure(1)
+        ax1 = fig.add_subplot(121)
+        ax1.imshow(obj_phase, cmap="bone")
+        ax1.axis("image")
+        ax1.set_title("Object with vortices")
+        ax1.plot(xres, yres, "or")
+        # display the probes after vortices removal
+        ax2 = fig.add_subplot(122)
+        ax2.imshow(np.angle(obj_phase_novort), cmap="bone")
+        ax2.axis("image")
+        ax2.set_title("Object - no vortices".format(1))
+        plt.show(block=False)
+
+    if params["overwrite_h5"]:
+        # only to keep a copy of the file and prevent overwritting
+        if not os.path.isfile(params["filename"] + ".vort"):
+            shutil.copy(params["filename"], params["filename"] + ".vort")
+        print("Overwritting probe information in the h5 file")
+        with h5py.File(params["filename"], "r+") as fid:
+            probe_new = fid["content/obj/S00G00/data"]
+            probe_new[...] = obj_phase_novort
--- toupy/restoration/vorticestools.py
+++ toupy/restoration/vorticestools.py
@@ -15,7 +15,14 @@ import h5py
 from ..restoration.ramptools import rmphaseramp
 from ..restoration.unwraptools import phaseresidues
 
-__all__ = ["cart2pol", "pol2cart", "rmvortices", "get_phase_novort"]
+__all__ = [
+    "cart2pol",
+    "get_object_novort",
+    "get_probe_novort",
+    "pol2cart",
+    "rmvortices_object",
+    "rmvortices_probe",
+]
 
 
 def cart2pol(x, y):
@@ -54,7 +61,93 @@ def pol2cart(rho, phi):
     y = rho * np.sin(phi)
     return (x, y)
 
-def get_phase_novort(img_phase, residues):
+
+def get_probe_novort(img_phase, residues):
+    """
+    Remove the vortices from the probe
+
+    Parameters
+    ----------
+    img_phase : array_like
+        Probe image with vortices to be removed without linear phase ramp
+    residues : array_like
+        Residues map
+
+    Returns
+    -------
+    img_phase_novort : array_like
+        Probe image without vortices
+    xres, yres : array_like
+        Coordinates `x` and `y` of the vortices
+    """
+    n, m = residues.shape
+    print("Getting vortice positions...")
+    yres, xres = np.nonzero(np.abs(residues) > 0.1)
+    X, Y = np.mgrid[: (m + 2), : (n + 2)]
+    # X and Y need to be transposed
+    X = X.T
+    Y = Y.T
+    for ii in range(len(xres)):
+        print("{} residues out of {}".format(ii + 1, len(xres)))
+        s0 = time.time()
+        yr = yres[ii]
+        xr = xres[ii]
+        T = ne.evaluate("arctan2(Y - yr, X - xr)")
+        rr = residues[yr, xr]
+        expr = ne.evaluate("exp(-1j*T*rr)")
+        img_phase_novort = ne.evaluate("img_phase * expr")
+        print("Time elapsed = {} s".format(time.time() - s0))
+    return img_phase_novort, xres, yres
+
+
+def rmvortices_probe(img_in, to_ignore=100):
+    """
+    Remove phase vortices on the probe image ignoring an amount of pixels
+    equals to ``to_ignore`` from the borders.
+    
+    Parameters
+    ----------
+    img_phase : array_like
+        Probe image with vortices to be removed.
+    to_ignore : int, optional
+        amount of pixels to ignore from the borders.
+
+    Returns
+    -------
+    img_phase_novort : array_like
+        Probe image without vortices
+    xres, yres : array_like
+        Coordinates `x` and `y` of the vortices
+
+    Note
+    ----
+    An eventual linear phase ramp will be remove from the input image.
+    """
+    # remove phase ramp
+    img_pr = rmphaseramp(img_in)  # [101:-100,93:-93] # to get square image
+    # find residues
+    residues, residues_charge = phaseresidues(np.angle(img_pr))
+
+    # ignore borders
+    if to_ignore != 0:
+        print("Ignoring border")
+        residues[:to_ignore, :] = 0
+        residues[:, :to_ignore] = 0
+        residues[-to_ignore:, :] = 0
+        residues[:, -to_ignore:] = 0
+
+    # initialize array
+    img_phase_vort = img_pr.copy().astype(np.complex64)
+
+    # remove vortices
+    img_phase_novort, xres, yres = get_probe_novort(img_phase_vort, residues)
+
+    img_phase_novort = rmphaseramp(img_phase_novort)
+
+    return img_phase_novort, xres, yres
+
+
+def get_object_novort(img_phase, residues):
     """
     Remove the vortices from the phase projections
 
@@ -75,12 +168,12 @@ def get_phase_novort(img_phase, residues):
     n, m = residues.shape
     print("Getting vortice positions...")
     yres, xres = np.nonzero(np.abs(residues) > 0.1)
-    X, Y = np.mgrid[:(m + 2),:(n + 2)]
+    X, Y = np.mgrid[: (m + 2), : (n + 2)]
     # X and Y need to be transposed
     X = X.T
     Y = Y.T
     for ii in range(len(xres)):
-        print('{} residues out of {}'.format(ii+1, len(xres)))
+        print("{} residues out of {}".format(ii + 1, len(xres)))
         s0 = time.time()
         yr = yres[ii]
         xr = xres[ii]
@@ -88,11 +181,12 @@ def get_phase_novort(img_phase, residues):
         rr = residues[yr, xr]
         expr = ne.evaluate("exp(1j*T*rr)")
         img_phase_novort = ne.evaluate("img_phase * expr")
-        print('Time elapsed = {} s'.format(time.time() - s0))
+        print("Time elapsed = {} s".format(time.time() - s0))
+        
     return img_phase_novort, xres, yres
 
 
-def rmvortices(img_in, to_ignore=100):
+def rmvortices_object(img_in, to_ignore=100):
     """
     Remove phase vortices on the object image ignoring an amount of pixels
     equals to ``to_ignore`` from the borders.
@@ -113,8 +207,7 @@ def rmvortices(img_in, to_ignore=100):
 
     Note
     ----
-    An eventual linear phase ramp will be remove from the input phase
-    image
+    An eventual linear phase ramp will be remove from the input image.
     """
     # remove phase ramp
     img_pr = rmphaseramp(img_in)  # [101:-100,93:-93] # to get square image
@@ -133,7 +226,9 @@ def rmvortices(img_in, to_ignore=100):
     img_phase_vort = img_pr.copy().astype(np.complex64)
 
     # remove vortices
-    img_phase_novort, xres, yres = get_phase_novort(img_phase_vort, residues)
+    img_phase_novort, xres, yres = get_object_novort(img_phase_vort, residues)
+
+    img_phase_novort = rmphaseramp(img_phase_novort)
 
     return img_phase_novort, xres, yres
 
@@ -179,7 +274,7 @@ def rmvortices_slow(img_in, to_ignore=100):
 
     # get array of vortice positions
     print("Getting vortice positions...")
-    #yres, xres = np.nonzero(np.abs(residues) > 0.1)
+    # yres, xres = np.nonzero(np.abs(residues) > 0.1)
     yres, xres = np.where(np.abs(residues) > 0.1)
     print("Found {}".format(len(xres)))
     # remove the vortices
